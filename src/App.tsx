45be5280a0228394276861f134dd2f934466bf03
rgsk <guptarahul70322@gmail.com>
2022-09-24 07:59:18 +0530

    refine tailwind tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { Refine } from "@pankod/refine-core";
-import routerProvider from "@pankod/refine-react-router-v6";
-import dataProvider from "@pankod/refine-simple-rest";
+import { Refine } from '@pankod/refine-core';
+import routerProvider from '@pankod/refine-react-router-v6';
+import dataProvider from '@pankod/refine-simple-rest';
+
+import { PostCreate, PostList, PostShow } from 'pages/posts';
+import { PostEdit } from 'pages/posts/edit';
+
+import { PostIcon } from 'icons';
+
+import { Layout } from 'components/Layout';
+
+import 'index.css';
 
 function App() {
   return (
-    <Refine
-      routerProvider={routerProvider}
-      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-    />
+    <div>
+      <Refine
+        routerProvider={routerProvider}
+        dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+        resources={[
+          {
+            name: "posts",
+            icon: PostIcon,
+            list: PostList,
+            show: PostShow,
+            edit: PostEdit,
+            create: PostCreate,
+          },
+        ]}
+        Layout={Layout}
+      />
+    </div>
   );
 }
 
---
+++ src/interfaces/index.d.ts
@@ -0,0 +1,13 @@
+export interface ICategory {
+  id: number;
+  title: string;
+}
+
+export interface IPost {
+  id: number;
+  title: string;
+  status: "published" | "draft" | "rejected";
+  createdAt: string;
+  category: { id: number };
+  content: string;
+}
